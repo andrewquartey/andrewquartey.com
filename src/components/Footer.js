b133d1c37088f1391c0692b4d3f545ee8a3cd1a8
Andrew Quartey <yestrup@gmail.com>
2018-10-03 22:50:58 +0000

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                <li><a href="https://www.facebook.com/andrew.quartey" target="_blank" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="https://www.twitter.com/andrequartz" target="_blank" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/andrewquartey/" target="_blank" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                    <li><a href="https://www.github.com/andrewquartey" target="_blank" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                    <li><a href="mailto:yestup@gmail.com" target="_blank" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                    <li>&copy; Yestrup Technologies</li><li>Design: <a href="http://gatsbyjs.org">Built with GatsbyJS</a></li>
                 </ul>
             </section>
         )
