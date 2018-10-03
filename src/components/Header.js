36f71bb452ed18c5cca1024945fa96737e77c989
Andrew Quartey <yestrup@gmail.com>
2018-10-03 22:04:56 +0000

    Header Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+import me from '../assets/images/me.png';
 class Header extends React.Component {
     render() {
         return (
             <section id="header">
                 <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
+                    <img src={me} width="150px" height="150px" alt="" />
+                    <h1>Hi, I'm <strong>Andrew Quartey</strong> a Computer Engineer<br />
+                    and a Fullstack Developer from <a href="https://en.wikipedia.org/wiki/Ghana">Ghana</a>.</h1>
+                    <p>I am very passionate about technology. <br />
+                        And it is my mission to change lives with it.</p>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
                     </ul>
