6baae09ccbd625d9aa11da9ffa563b4334533398
Andrew Quartey <yestrup@gmail.com>
2018-10-24 11:48:06 +0000

    Update Email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                     <li><a href="https://www.twitter.com/andrequartz" target="_blank" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                     <li><a href="https://www.linkedin.com/in/andrewquartey/" target="_blank" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
                     <li><a href="https://www.github.com/andrewquartey" target="_blank" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="mailto:yestup@gmail.com" target="_blank" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="mailto:andrew@yestruptech.com" target="_blank" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; Yestrup Technologies</li><li>Design: <a href="http://gatsbyjs.org">Built with GatsbyJS</a></li>
